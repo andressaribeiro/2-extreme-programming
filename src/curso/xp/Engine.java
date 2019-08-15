0bf22fb8d353c09289dc51f5d72c7157c7b36d52
Ana Albuquerque <albuquerque.apr@gmail.com>
2019-08-15 11:38:38 -0300

    .

--- src/curso/xp/Engine.java
+++ src/curso/xp/Engine.java
@@ -3,9 +3,14 @@ package curso.xp;
 import model.Player;
 
 public class Engine {
-
+    
     public void printPlayerPosition(Player player) {
         System.out.println(String.format("x: %d, y: %d", player.getPosition().x, player.getPosition().y));
+    }
+
+    public String[][] getCenario() {
+        String[][] cenario = new String[20][20];
+        return cenario;
     }
 
 }
