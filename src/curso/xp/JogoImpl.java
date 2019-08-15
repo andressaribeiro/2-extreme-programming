10be4143f32b889c5324cc1fd1b7882de40033dc
Fernando Mota <fmaken@gmail.com>
2019-08-15 16:20:06 -0300

    wip

--- src/curso/xp/JogoImpl.java
+++ src/curso/xp/JogoImpl.java
@@ -12,6 +12,13 @@ public class JogoImpl implements Jogo {
 
     private Integer score = 0;
 
+<<<<<<< Updated upstream
+=======
+    private void startCenario() {
+
+    }
+
+>>>>>>> Stashed changes
     public String[][] refresh() {
         if (cenario == null) {
             cenario = new String[5][5];
